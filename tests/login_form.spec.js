2f5be6e4f3ef4391426655391bf163074e220cd0
salfonsoMB17 <serggios17@gmail.com>
2026-08-22 17:57:21 -0500

    Exercise 5.18: Add E2E tests for login with correct and wrong credentials

--- tests/login_form.spec.js
+++ tests/login_form.spec.js
@@ -1,11 +1,37 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
     await page.goto('http://localhost:5173')
+    await request.post('http://localhost:3003/api/testing/reset')
+    await request.post('http://localhost:3003/api/users/', {
+      data: {
+        username: 'mluukkai',
+        name: 'Matti Luukkainen',
+        password: 'secret'
+      }
+    })
   })
 
   test('Login form is shown', async ({ page }) => {
     await expect(page.getByText('Log in to application')).toBeVisible()
+  })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+    await page.getByRole('textbox').first().fill('mluukkai')
+    await page.getByRole('textbox').last().fill('secret')
+    await page.getByRole('button', { name: 'login' }).click()  
+    
+    await expect(page.getByText('Matti Luukkainen logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await page.getByRole('textbox').first().fill('mluukkai')
+      await page.getByRole('textbox').last().fill('wrong')
+      await page.getByRole('button', { name: 'login' }).click()
+
+      await expect(page.getByText('wrong credentials')).toBeVisible()
+    })
   })
 })
